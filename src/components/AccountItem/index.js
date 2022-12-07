1a0029bbd2706d6555a0b3acc8b4f50dbadf0308
trungquan2609 <nguyentrungquan.2609@gmail.com>
2022-12-08 00:09:21 +0700

    search-wrapper upload login btn

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,29 @@
+import classNames from 'classnames/bind';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+function AccountIem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/cc169321062db1e5b2ad0e89e4caa8d0~c5_100x100.jpeg?x-expires=1670554800&x-signature=wWTLuZ9ei12pAYqhuk0TA2t4t30%3D"
+                alt="vietphuonghoa98"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon
+                        className={cx('check')}
+                        icon={faCheckCircle}
+                    />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountIem;
